30f948b03539b9171523fc306d42b6fd5fbf9857
Wei Ding <bailuo.work@gmail.com>
2021-02-28 14:26:43 +0800

    add solution to question 100.same-tree

---
+++ leetcode/100.same-tree.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=100 lang=javascript
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/* 
+Accepted
+60/60 cases passed (76 ms)
+Your runtime beats 82.52 % of javascript submissions
+Your memory usage beats 14.05 % of javascript submissions (39.2 MB) */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    if (p === null && q === null) {
+        return true;
+    }
+    if (p && q && p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    return false;
+};
+// @lc code=end
