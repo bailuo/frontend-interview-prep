24ee3e9fd16d8c1f620dadb18de87362fd9e5573
Wei Ding <bailuo.work@gmail.com>
2021-04-18 14:31:59 +0800

    two soultions on quiestion 110

--- leetcode/110.balanced-binary-tree.js
+++ leetcode/110.balanced-binary-tree.js
@@ -17,13 +17,28 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// df = [height of left child - height 0f right child]
+// 平衡二叉树成立条件，左右高度差不能大于1且子树需要同时为平衡二叉树
+
+// top to boyyom
 var isBalanced = function (root) {
+    // return isBalancedTopToBottom(root);
+    return isBalancedBottomToTop(root) !== -1;
+};
+/* 
+Accepted
+228/228 cases passed (168 ms)
+Your runtime beats 5.04 % of javascript submissions
+Your memory usage beats 15.61 % of javascript submissions (43.8 MB) */
+
+function isBalancedTopToBottom(root) {
     if (!root) {
         return true;
     }
-    return Math.abs(calculateTreeHeight(root.left) - calculateTreeHeight(root.right)) <= 1;
-};
+    // df = [height of left child - height 0f right child]
+    const df = Math.abs(calculateTreeHeight(root.left) - calculateTreeHeight(root.right));
+    const isSubTreeValid = isBalanced(root.left) && isBalanced(root.right);
+    return df <= 1 && isSubTreeValid;
+}
 
 function calculateTreeHeight(node) {
     if (!node) {
@@ -31,6 +46,22 @@ function calculateTreeHeight(node) {
     }
     return Math.max(calculateTreeHeight(node.left), calculateTreeHeight(node.right)) + 1;
 }
-// @lc code=end
+/* 
 
-// I havent finsih this question yet, just save it for termproary
+Accepted
+228/228 cases passed (100 ms)
+Your runtime beats 31.32 % of javascript submissions
+Your memory usage beats 75.59 % of javascript submissions (43 MB) */
+function isBalancedBottomToTop(root) {
+    if (!root) {
+        return 0;
+    }
+    const left = isBalancedBottomToTop(root.left);
+    const right = isBalancedBottomToTop(root.right);
+    //return early if we found a non-balanced subtree
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1;
+    }
+    return Math.max(left, right) + 1;
+}
+// @lc code=end
