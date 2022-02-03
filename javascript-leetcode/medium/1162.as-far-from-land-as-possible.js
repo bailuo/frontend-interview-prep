41b420019d7687143331adb1599c65d169497fc8
Conan.W.Ding <bailuo.work@gmail.com>
2022-02-04 02:22:21 +0800

    Leetcode practice (#13)
    * feat: problem1162 solution multi-source BFS
    
    * feat: add comments to 567 and solution to 1414 and 1765

--- javascript-leetcode/medium/1162.as-far-from-land-as-possible.js
+++ javascript-leetcode/medium/1162.as-far-from-land-as-possible.js
@@ -10,13 +10,18 @@
  * @return {number}
  */
 var maxDistance = function (grid) {
-    return maxDistanceOne(grid);
+    return maxDistanceTwo(grid);
 };
+
 /*
  * Accepted
  * 35/35 cases passed (1688 ms)
  * Your runtime beats 10.53 % of javascript submissions
  * Your memory usage beats 28.95 % of javascript submissions (54.3 MB)
+ *
+ *  One Source BFS
+ *  a basic logic is to explore all cells with a BFS approach, from water cells, we count each step when we move
+ *
  * */
 const maxDistanceOne = function (grid) {
     let ans = -1;
@@ -42,16 +47,21 @@ const maxDistanceBFS = function (grid, r, c) {
     const map = new Map();
     queue.unshift([r, c]);
     // r* rowLength + c is to make it easier to index
+    // set initial step to be 0
     map.set(r * rowLength + c, 0);
     while (queue.length) {
         const [currentRow, currentCol] = queue.pop();
         const step = map.get(currentRow * rowLength + currentCol);
+        // if the value equal to 1, it means we reached a land,
+        // so we just return the current step count, there is not need to continue
         if (grid[currentRow][currentCol] === 1) {
             return step;
         }
+        // otherwise, we explore four connected cells
         dirs.forEach((_dir) => {
             const _dirRow = currentRow + _dir[0];
             const _dirCol = currentCol + _dir[1];
+            // examine whether the cell is out of range
             if (_dirRow < 0 || _dirRow >= rowLength || _dirCol < 0 || _dirCol >= colLength) {
                 return;
             }
@@ -60,10 +70,65 @@ const maxDistanceBFS = function (grid, r, c) {
             if (map.has(key)) {
                 return;
             }
+            // put the cell in the queue
             queue.unshift([_dirRow, _dirCol]);
+            // we haven't reached a land yet, so we add one more step
             map.set(key, step + 1);
         });
     }
     return -1;
+};
+/*
+ * convert multi-source to one source
+ * Find land and try to reach water cell from land, the remember distance to water from land
+ *
+ * Accepted
+ * 35/35 cases passed (240 ms)
+ * Your runtime beats 34.21 % of javascript submissions
+ * Your memory usage beats 28.95 % of javascript submissions (54.5 MB)
+ * */
+const maxDistanceTwo = function (grid) {
+    const rowLen = grid.length;
+    const colLen = grid[0].length;
+    const queue = [];
+    const stepMap = new Map();
+    let ans = -1;
+    for (let row = 0; row < rowLen; row++) {
+        for (let col = 0; col < colLen; col++) {
+            if (grid[row][col] === 1) {
+                // push all land into the queue
+                queue.unshift([row, col]);
+                stepMap.set(row * rowLen + col, 0);
+            }
+        }
+    }
+    const dirs = [
+        [1, 0],
+        [-1, 0],
+        [0, -1],
+        [0, 1],
+    ];
+    while (queue.length) {
+        const [r, c] = queue.pop();
+        const step = stepMap.get(r * rowLen + c);
+        dirs.forEach((_dir) => {
+            const _r = r + _dir[0];
+            const _c = c + _dir[1];
+            if (_r < 0 || _r >= rowLen || _c < 0 || _c >= colLen) {
+                return;
+            }
+            // the grid is visited already, continue
+            if (grid[_r][_c] !== 0) {
+                return;
+            }
+            const cellStep = step + 1;
+            // mark as visited
+            grid[_r][_c] = cellStep;
+            queue.unshift([_r, _c]);
+            stepMap.set(_r * rowLen + _c, cellStep);
+            ans = Math.max(ans, cellStep);
+        });
+    }
+    return ans;
 };
 // @lc code=end
--- javascript-leetcode/medium/567.permutation-in-string.js
+++ javascript-leetcode/medium/567.permutation-in-string.js
@@ -10,7 +10,6 @@
  * @param {string} s2
  * @return {boolean}
  */
-// exceed of time limits
 /* Accepted
  * 106/106 cases passed (4433 ms)
  * Your runtime beats 11.08 % of javascript submissions
@@ -24,15 +23,20 @@ var checkInclusion = function (s1, s2) {
     if (s2.length < s1.length) {
         return false;
     }
+    // get all characters of s1 in the map
     const s1Map = s1.split('').reduce((accu, curr) => {
         accu[curr] = undefined === accu[curr] ? 1 : accu[curr] + 1;
         return accu;
     }, {});
+    // make substring of s2 and check if the substring has all characters of s1
     while (fast < s2.length) {
+        // the substring should be as long as s1, the fast pointer move right
         fast = fast < s1.length - 1 ? s1.length - 1 : fast + 1;
+        // if the length is more than s1.length, shrink the left border of s2 substring
         if (fast - slow + 1 > s1.length) {
             slow += 1;
         }
+        // generate the charachter map of s2 substring
         const tempMap = s2
             .substring(slow, fast + 1)
             .split('')
@@ -40,13 +44,14 @@ var checkInclusion = function (s1, s2) {
                 accu[curr] = undefined === accu[curr] ? 1 : accu[curr] + 1;
                 return accu;
             }, {});
+        // compare the two maps, if equal, return true
         if (hashCompare(s1Map, tempMap) === true) {
             return true;
         }
     }
     return false;
 };
-
+// the comparte function
 function hashCompare(map1, map2) {
     for (const key in map1) {
         if (undefined === map2[key] || map1[key] !== map2[key]) {
